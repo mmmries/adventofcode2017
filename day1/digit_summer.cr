d514dad66d70be2f67e5ed9d722d4160f646b9b9
Michael Ries <michael@riesd.com>
2017-12-05 15:43:18 -0700

    day 1 - part 2

--- day1/digit_summer.cr
+++ day1/digit_summer.cr
@@ -1,9 +1,19 @@
 class DigitSummer
-  def self.sum_matching(digits)
+  def self.sum_matching_neighbors(digits)
     digits = digits + [digits[0]]
     sum = 0
     digits.each_cons(2) do |(a, b)|
       sum += a if a == b
+    end
+    sum
+  end
+
+  def self.sum_matching_opposites(digits)
+    sum = 0
+    half_way_round = digits.size / 2
+    digits.each_with_index do |digit, index|
+      opposite_index = (index + half_way_round) % digits.size
+      sum += digit if digit == digits[opposite_index]
     end
     sum
   end
--- day1/digit_summer_spec.cr
+++ day1/digit_summer_spec.cr
@@ -2,10 +2,18 @@ require "spec"
 require "./digit_summer"
 
 describe DigitSummer do
-  it "filters to only consider matching digits" do
-    DigitSummer.sum_matching([1,1,2,2]).should eq 3
-    DigitSummer.sum_matching([1,1,1,1]).should eq 4
-    DigitSummer.sum_matching([1,2,3,4]).should eq 0
-    DigitSummer.sum_matching([9,1,2,1,2,1,2,9]).should eq 9
+  it "sums matching neighbor digits" do
+    DigitSummer.sum_matching_neighbors([1,1,2,2]).should eq 3
+    DigitSummer.sum_matching_neighbors([1,1,1,1]).should eq 4
+    DigitSummer.sum_matching_neighbors([1,2,3,4]).should eq 0
+    DigitSummer.sum_matching_neighbors([9,1,2,1,2,1,2,9]).should eq 9
+  end
+
+  it "sums matching opposites" do
+    DigitSummer.sum_matching_opposites([1,2,1,2]).should eq 6
+    DigitSummer.sum_matching_opposites([1,2,2,1]).should eq 0
+    DigitSummer.sum_matching_opposites([1,2,3,4,2,5]).should eq 4
+    DigitSummer.sum_matching_opposites([1,2,3,1,2,3]).should eq 12
+    DigitSummer.sum_matching_opposites([1,2,1,3,1,4,1,5]).should eq 4
   end
 end
